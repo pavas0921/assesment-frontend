0b48b22fe0c0202a3e925f13c35060478d32cfb9
Carlos Andrés Pavas Correa <pavas0921@gmail.com>
2023-03-18 19:28:30 -0500

    Se ubica navbar en la parte superior centar de la pantalla y se elimina opción product details

--- src/components/HomeMenu/HomeMenu.jsx
+++ src/components/HomeMenu/HomeMenu.jsx
@@ -7,9 +7,7 @@ const HomeMenu = () => {
 			<div className="nav__div-buttons">
 				<button className="nav___buttons">Home</button>
 			</div>
-			<div className="nav__div-buttons">
-				<button className="nav___buttons">Products Details</button>
-			</div>
+
 			<div className="nav__div-buttons">
 				<button className="nav___buttons">About</button>
 			</div>
