eef9042069312be92e41dd7d5cb2a7e5216a4f79
Carlos Andrés Pavas Correa <pavas0921@gmail.com>
2023-03-27 19:04:05 -0500

    Creación de api structure

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getAllProducts } from "../../services/productsAPI";
 import "./product_list.scss";
 
 const ProducList = () => {
-	return <div className="product-list">lista productos</div>;
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		const getData = async () => {
+			const data = await getAllProducts();
+			setProducts(data);
+		};
+
+		getData();
+	}, []);
+	console.log(products);
+	return <div>products</div>;
 };
 
 export default ProducList;
